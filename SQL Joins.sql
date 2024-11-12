4944931bfc265621526131d30d3aefaef7d113c7
Anjali Sharma <anjalish9111@gmail.com>
2024-11-12 15:17:07 +0530

    Update SQL Joins.sql

--- SQL Joins.sql
+++ SQL Joins.sql
@@ -1,7 +1,3 @@
-SELECT category.name AS category_name, film.title AS film_title
-FROM film
-RIGHT JOIN film_category ON film.film_id = film_category.film_id
-RIGHT JOIN category ON film_category.category_id = category.category_id;
 
 
 
